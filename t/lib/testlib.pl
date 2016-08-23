c4c1f2bdd11b580b596cb9e95ec3776e3542afd9
perlancar (@pc-office) <perlancar@gmail.com>
2016-08-23 21:47:52 +0700

    Replace dumping all installed modules when diagnosing require test failure to just printing the require'd module's path

--- t/lib/testlib.pl
+++ t/lib/testlib.pl
@@ -1,7 +1,6 @@
 use File::Temp qw(tempfile);
+use Module::Path::More qw(module_path);
 use Test::More 0.98;
-
-my $already_listed_modules;
 
 sub _test_lib {
     my $which = shift;
@@ -26,20 +25,13 @@ sub _test_lib {
             my $child_err = $?;
             if ($ent->{ok}) {
                 unless (ok(!$child_err, "require $ent->{module} ok")) {
-                    if (!$already_listed_modules++) {
-                        local @INC = (
-                            @{ $args{extra_lib} || [] },
-                            @main::ORIG_INC,
-                        );
-                        if (eval { require PERLANCAR::Module::List; 1 }) {
-                            diag explain PERLANCAR::Module::List::list_modules(
-                                "", {
-                                    list_modules=>1,
-                                    recurse=>1,
-                                    return_path=>1},
-                            );
-                        }
-                    }
+                    local @INC = (
+                        @{ $args{extra_lib} || [] },
+                        @main::ORIG_INC,
+                    );
+                    my $path = module_path(module => $ent->{module});
+                    diag "Path for module $ent->{module}: " .
+                        ($path // "(not found)");
                 }
             } else {
                 ok( $child_err, "require $ent->{module} nok");
